99d7fc5cb9520f016602bddff300d0ae1d7a510d
jjheffernan <jjheffernan99@gmail.com>
2022-07-13 10:59:41 -0400

    completed rudementary conversion pipeline. Working on control flow and examples.

--- firstpipeline.py
+++ firstpipeline.py
@@ -1,10 +1,25 @@
 # first pipeline
 
 import json
+import sys
 import yaml
 
+
+def is_yaml(filename):
+	return False
+
+
+def is_json(filename):
+	return True
+
+
+def do_yaml_to_json(filename):
+	# pass
+	return 'yaml' in filename
+
+
 def do_json_to_yaml(filename):
-	pass
+
 	print('do json to yaml')
 	f = open(filename)
 	# step 2
@@ -15,11 +30,23 @@ def do_json_to_yaml(filename):
 	output_filename = filename.replace('.json', '.yaml')
 	print('output: ', output_filename)
 	file_out = open(output_filename, "w")
-	file_out.write(yaml.dump(data)
-	f.close()	
+	file_out.write(yaml.dump(data))
+	f.close()
 	file_out.close()
 
 
-if __name__ = '__main__':
-	filename = 'donuts.json'
-	do_json_to_yaml(filename)
+if __name__ == '__main__':
+	# filename = 'donuts.json'
+	if len(sys.argv) != 2:
+		print("not enough arguments: ")
+		exit()
+	print('args ', sys.argv)
+	filename = sys.argv[1]
+	# filename = str(sys.argv)
+	if is_yaml(filename):
+		do_yaml_to_json(filename)
+	elif is_json(filename):
+		do_json_to_yaml(filename)
+	else:
+		print('incompatible file')
+	# do_json_to_yaml(filename)
